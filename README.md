6c8f015d74e442baf8b58d589e3c3eac4f1b5a73
Moha-AlHanbali <Moha.AlHanbali@gmail.com>
2021-06-22 15:23:25 +0300

    added lab03 req

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # code201_About_Me
+
+## About Me lab02
+
+- Added Quiz about myself
+- Added Bio
+
+<br>
+
+## About Me lab03
+
--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 `use strict`;
 
+let userscore = 0
+
 alert("Hello! Welcome to this Webpage!");
 
 let username= prompt("What is your name?");
@@ -19,6 +21,7 @@ switch(q1a) {
     case "no":
     case "n":
         alert("Hope we would in the future!");
+        userscore++;
         // console.log("Hope we would in the future!");
         break;
     default:
@@ -33,6 +36,7 @@ switch(q2a) {
     case "yes":
     case "y":
         alert("Correct! I love music!");
+        userscore++;
         // console.log("Correct! I love music!");
         break;
     case "no":
@@ -51,12 +55,13 @@ let q3a= prompt("Do you think my favorite color is blue?").toLowerCase();
 switch(q3a) {
     case "yes":
     case "y":
-        alert("Well, it is one of my preferred colours but not my favorte!");
+        alert("Well, it is one of my preferred colors but not my favorte!");
         // console.log("Well, it is one of my preferred colours but not my favorte!");
         break;
     case "no":
     case "n":
         alert("Right! My favorite colour is Black!");
+        userscore++;
         // console.log("Right! My favorite colour is Black!");
         break;
     default:
@@ -76,6 +81,7 @@ switch(q4a) {
     case "no":
     case "n":
         alert("This is the truth, sadly..");
+        userscore++;
         // console.log("This is the truth, sadly..");
         break;
     default:
@@ -91,6 +97,7 @@ switch(q5a) {
     case "yes":
     case "y":
         alert("Correct! But I'm always too lazy to read..");
+        userscore++;
         // console.log("Correct! But I'm always too lazy to read..");
         break;
     case "no":
@@ -103,5 +110,53 @@ switch(q5a) {
          // console.log("Please answer the next question with (yes/no).");
         break;
 }
+
+alert("How about we try something else!");
+
+
+
+let mynumber = 77;
+
+for (i=0; i<4 ; i++){
+        guess=Number(prompt("Guess the number I have in my mind, it is between (1-100)!"));
+
+            if (mynumber > guess){
+                alert("Incorrect, your guess is too low!" + " You have " + Number(3-i) + " attempts left.")
+                // console.log("Incorrect, your guess is too low!" + " You have " + Number(3-i) + " attempts left.");
+            } else if (mynumber < guess){
+                alert("Incorrect, your guess is too high!" + " You have " + Number(3-i) + " attempts left.")} 
+                // console.log("Incorrect, your guess is too high!" + " You have " + Number(3-i) + " attempts left.");
+                else{
+                alert("Correct! Well done!");
+                // console.log("Correct! Well done!");
+                userscore++;
+                break;
+            }
+}
+
+alert("The only correct answer is 77!");
+
+alert("Okay, next question!");
+
+
+
+let mynumbers = [12, 19, 25, 95];
+
+for (i=0; i<5; i++){
+    let guess2=Number(prompt("Can you guess any number related to me?"))
+    //  if (mynumbers.includes(guess2))
+        if (guess2 === mynumbers[0] || guess2 === mynumbers[1] || guess2 === mynumbers[2] || guess2 === mynumbers[3]){
+        alert("Correct! " + guess2 + " is one of them, good job!");
+        // console.log("Correct! " + guess2 + " is one of them, good job!");
+        userscore++;
+        break;
+    } else{
+        alert("Incorrect, try again!" + " You have " + Number(5-i) + " attempts left.")
+    }
+}
+
+alert("The correct answers are (12 ,19, 25, and 95).");
+
+alert("Your total score is " + userscore +"/7");
 
 alert("Okay " + username + ", we're done for now! Thank you for taking time!");
