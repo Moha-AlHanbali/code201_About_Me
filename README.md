5c4eae7fdec190a4db3a51762fe34255a3d047bc
Moha-AlHanbali <Moha.AlHanbali@gmail.com>
2021-06-24 14:27:43 +0300

    formatted code and added CSS

--- README.md
+++ README.md
@@ -5,10 +5,17 @@
 - Added Quiz about myself
 - Added Bio
 
-<br>
-
 ## About Me lab03
 
 - Added more questions
 - Added Top 10 List
 - Planning on achieving stretch goals later
+
+## About Me lab04
+
+- Pair-Programming- Added Functions (by Naziha)
+
+## About Me lab05b
+
+- Added more styling
+- Formatted the Documents
--- js/app.js
+++ js/app.js
@@ -1,7 +1,178 @@
 // eslint-disable-next-line quotes
 `use strict`;
 
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function q1f(){
+
+  let q1a= prompt('Have we ever met before, ' + username + '?').toLowerCase();
+
+  switch(q1a) {
+  case 'yes':
+  case 'y':
+    alert('Oh! I don\'t think I remember that, sadly!');
+    break;
+  case 'no':
+  case 'n':
+    alert('Hope we would in the future!');
+    userscore++;
+    break;
+  default:
+    alert('Please answer the next question with (yes/no).');
+    break;
+  }
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function q2f (){
+
+  let q2a= prompt('Do you think I like music?').toLowerCase();
+
+  switch(q2a) {
+  case 'yes':
+  case 'y':
+    alert('Correct! I love music!');
+    userscore++;
+    break;
+  case 'no':
+  case 'n':
+    alert('Incorrect! I actually do like music!');
+    break;
+  default:
+    alert('Please answer the next question with (yes/no).');
+    break;
+  }
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function q3f(){
+
+  let q3a= prompt('Do you think my favorite color is blue?').toLowerCase();
+
+  switch(q3a) {
+  case 'yes':
+  case 'y':
+    alert('Well, it is one of my preferred colors but not my favorte!');
+    break;
+  case 'no':
+  case 'n':
+    alert('Right! My favorite colour is Black!');
+    userscore++;
+    break;
+  default:
+    alert('Please answer the next question with (yes/no).');
+    break;
+  }
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function q4f() {
+
+  let q4a= prompt('Guess this one, do I spend a moderate amount of time on the internet?').toLowerCase();
+
+  switch(q4a) {
+  case 'yes':
+  case 'y':
+    alert('I appreciate your good will, but this isn\'t true..');
+    break;
+  case 'no':
+  case 'n':
+    alert('This is the truth, sadly..');
+    userscore++;
+    break;
+  default:
+    alert('Please answer the next question with (yes/no).');
+    break;
+  }
+}
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function q5f() {
+
+  let q5a= prompt('Finally, do you think I was ever into reading?').toLowerCase();
+
+  switch(q5a) {
+  case 'yes':
+  case 'y':
+    alert('Correct! But I\'m always too lazy to read..');
+    userscore++;
+    break;
+  case 'no':
+  case 'n':
+    alert('Not really, but has some truth into it.');
+    break;
+  default:
+    alert('Please answer the next question with (yes/no).');
+    break;
+  }
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
+function q6f(){
+
+  for (let i=0; i<4 ; i++){
+    let guess=Number(prompt('Guess the number I have in my mind, it is between (1-100)!'));
+
+    if (mynumber > guess){
+      alert('Incorrect, your guess is too low!' + ' You have ' + Number(3-i) + ' attempts left.');
+    } else if (mynumber < guess){
+      alert('Incorrect, your guess is too high!' + ' You have ' + Number(3-i) + ' attempts left.');}
+    else{
+      alert('Correct! Well done!');
+      userscore++;
+      break;
+    }
+  }
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function q7f(){
+  for (let i=0; i<5; i++){
+
+    let guess2=Number(prompt('Can you guess any number related to me?'));
+
+    //  if (mynumbers.includes(guess2))
+    //  if (guess2 === mynumbers[0] || guess2 === mynumbers[1] || guess2 === mynumbers[2] || guess2 === mynumbers[3])
+
+    for (let j=0; j<mynumbers.length; j++ ){
+
+
+      if(guess2 === mynumbers[j]){
+        alert('Correct! ' + guess2 + ' is one of them, good job!');
+
+        userscore++;
+
+        breaker = true;
+
+        break;}
+    }
+    if (breaker === true){
+      break;
+    } else {alert('Incorrect, try again!' + ' You have ' + Number(5-i) + ' attempts left.');
+    }
+  }
+}
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
 let userscore = 0;
+let mynumber = 77;
+let mynumbers = [12, 19, 25, 95];
+let breaker = false;
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
 alert('Hello! Welcome to this Webpage!');
 
@@ -10,187 +181,27 @@ let username= prompt('What is your name?');
 alert('Hello ' + username + ', I hope you\'re well!');
 
 alert('I will ask you few questions ' + username + ', please answer them with (yes/no).');
-function q1f(){
-let q1a= prompt('Have we ever met before, ' + username + '?').toLowerCase();
 
-switch(q1a) {
-case 'yes':
-case 'y':
-  alert('Oh! I don\'t think I remember that, sadly!');
-  // console.log("Oh! I don't think I remember that, sadly!");
-  break;
-case 'no':
-case 'n':
-  alert('Hope we would in the future!');
-  userscore++;
-  // console.log("Hope we would in the future!");
-  break;
-default:
-  alert('Please answer the next question with (yes/no).');
-  // console.log("Please answer the next question with (yes/no).");
-  break;
-}
-}
 q1f();
-function q2f (){
-let q2a= prompt('Do you think I like music?').toLowerCase();
 
-switch(q2a) {
-case 'yes':
-case 'y':
-  alert('Correct! I love music!');
-  userscore++;
-  // console.log("Correct! I love music!");
-  break;
-case 'no':
-case 'n':
-  alert('Incorrect! I actually do like music!');
-  // console.log("Incorrect! I actually do like music!");
-  break;
-default:
-  alert('Please answer the next question with (yes/no).');
-  // console.log("Please answer the next question with (yes/no).");
-  break;
-}
-}
 q2f();
-function q3f(){
-let q3a= prompt('Do you think my favorite color is blue?').toLowerCase();
 
-switch(q3a) {
-case 'yes':
-case 'y':
-  alert('Well, it is one of my preferred colors but not my favorte!');
-  // console.log("Well, it is one of my preferred colours but not my favorte!");
-  break;
-case 'no':
-case 'n':
-  alert('Right! My favorite colour is Black!');
-  userscore++;
-  // console.log("Right! My favorite colour is Black!");
-  break;
-default:
-  alert('Please answer the next question with (yes/no).');
-  // console.log("Please answer the next question with (yes/no).");
-  break;
-}
-}
 q3f();
 
-function q4f() {
-  
-
-let q4a= prompt('Guess this one, do I spend a moderate amount of time on the internet?').toLowerCase();
-
-switch(q4a) {
-case 'yes':
-case 'y':
-  alert('I appreciate your good will, but this isn\'t true..');
-  // console.log("I appreciate your good will, but this isn't true..");
-  break;
-case 'no':
-case 'n':
-  alert('This is the truth, sadly..');
-  userscore++;
-  // console.log("This is the truth, sadly..");
-  break;
-default:
-  alert('Please answer the next question with (yes/no).');
-  // console.log("Please answer the next question with (yes/no).");
-
-  break;
-}
-}
 q4f();
 
-function q5f() {
-  
-
-let q5a= prompt('Finally, do you think I was ever into reading?').toLowerCase();
-
-switch(q5a) {
-case 'yes':
-case 'y':
-  alert('Correct! But I\'m always too lazy to read..');
-  userscore++;
-  // console.log("Correct! But I'm always too lazy to read..");
-  break;
-case 'no':
-case 'n':
-  alert('Not really, but has some truth into it.');
-  // console.log("Not really, but has some truth into it.");
-  break;
-default:
-  alert('Please answer the next question with (yes/no).');
-  // console.log("Please answer the next question with (yes/no).");
-  break;
-}
-}
 q5f();
 
 alert('How about we try something else!');
 
-
-let mynumber = 77;
-function q6f(){
-
-for (let i=0; i<4 ; i++){
-  let guess=Number(prompt('Guess the number I have in my mind, it is between (1-100)!'));
-
-  if (mynumber > guess){
-    alert('Incorrect, your guess is too low!' + ' You have ' + Number(3-i) + ' attempts left.');
-    // console.log("Incorrect, your guess is too low!" + " You have " + Number(3-i) + " attempts left.");
-  } else if (mynumber < guess){
-    alert('Incorrect, your guess is too high!' + ' You have ' + Number(3-i) + ' attempts left.');}
-  // console.log("Incorrect, your guess is too high!" + " You have " + Number(3-i) + " attempts left.");
-  else{
-    alert('Correct! Well done!');
-    // console.log("Correct! Well done!");
-    userscore++;
-    break;
-  }
-}
-} 
 q6f();
 
 alert('The only correct answer is 77!');
 
 alert('Okay, next question!');
 
-
-
-let mynumbers = [12, 19, 25, 95];
-let breaker = false;
-function q7f(){
-for (let i=0; i<5; i++){
-
-  let guess2=Number(prompt('Can you guess any number related to me?'));
-
-  //  if (mynumbers.includes(guess2))
-  //  if (guess2 === mynumbers[0] || guess2 === mynumbers[1] || guess2 === mynumbers[2] || guess2 === mynumbers[3])
-
-  for (let j=0; j<mynumbers.length; j++ ){
-
-
-    if(guess2 === mynumbers[j]){
-      alert('Correct! ' + guess2 + ' is one of them, good job!');
-
-      // console.log("Correct! " + guess2 + " is one of them, good job!");
-
-      userscore++;
-
-      breaker = true;
-
-      break;}
-  }
-  if (breaker === true){
-    break;
-  } else {alert('Incorrect, try again!' + ' You have ' + Number(5-i) + ' attempts left.');
-  }
-}
-
-}
 q7f();
+
 alert('The correct answers are (12 ,19, 25, and 95).');
 
 alert('Your total score is ' + userscore +'/7');
